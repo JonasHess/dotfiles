375d24f2161bda19ebac32f66aa07241302fbdc2
Jonas Hess <j.hess@mvb-online.de>
2026-08-21 12:24:00 +0200

    nvim - use carbonfox dark colorscheme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,5 @@
+return {
+  -- carbonfox: very dark near-black background (#161616), darker than the tokyonight default.
+  { "EdenEast/nightfox.nvim", priority = 1000 },
+  { "LazyVim/LazyVim", opts = { colorscheme = "carbonfox" } },
+}
